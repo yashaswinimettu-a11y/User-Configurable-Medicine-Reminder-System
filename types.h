3515c3fe6d8dea4dac5cfe9ab63e370618b12353
yashaswinimettu-a11y <yashaswinimettu@gmail.com>
2026-08-18 07:03:18 +0530

    Delete types.h

--- types.h
+++
@@ -1,8 +0,0 @@
-typedef unsigned char u8;
-typedef char s8;
-typedef unsigned int u32;
-typedef int s32;
-typedef unsigned short int u16;
-typedef signed short int s16;
-typedef float f32;
-typedef double d64;
